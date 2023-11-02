668a32ce94540ed350db68345418bf4a583b031c
Noah Calhoun <w.noah.calhoun@gmail.com>
2023-11-02 13:07:37 -0500

    added get tests

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # WMT_GO_API
 This program acts as an API server, allowing for CRUD operations to be made.
 Data is stored in local storage via JSON files. 
+If tests start failing, delete contents of alerts folder.
 
 To run the server:
 > go run main.go
 
 Run tests:
-Start the server.
+Start the server:
+> go run main.go
 In a new terminal, run
-> 
+> go test
--- unit_test.go
+++ unit_test.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"net/http"
 	"os"
+	"strconv"
 	"testing"
+	"time"
 )
 
 func TestPost1(t *testing.T) {
@@ -189,6 +191,171 @@ func TestPost5(t *testing.T) {
 	// Verify the response status code
 	if http.StatusInternalServerError != resp.StatusCode {
 		t.Errorf("Expected status code %d, but got %d", http.StatusInternalServerError, resp.StatusCode)
+	}
+}
+
+func TestGet1(t *testing.T) {
+	serverURL := "http://localhost:8080"
+	client := &http.Client{}
+
+	// Create a GET request with the desired query parameters
+	req, err := http.NewRequest("GET", serverURL+"/alerts?service_id=my_test_service_id&start_ts=0&end_ts=9999999999", nil)
+	if err != nil {
+		t.Errorf("Error creating GET request: %v", err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Error sending GET request: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Verify the response status code
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, resp.StatusCode)
+	}
+}
+
+func TestGet2(t *testing.T) {
+	serverURL := "http://localhost:8080"
+	client := &http.Client{}
+
+	// Create a GET request with the desired query parameters
+	req, err := http.NewRequest("GET", serverURL+"/alerts?service_id=my_test_service_id&start_ts=0&end_ts=9999999999", nil)
+	if err != nil {
+		t.Errorf("Error creating GET request: %v", err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Error sending GET request: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Verify the response status code
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, resp.StatusCode)
+	}
+}
+
+func TestGetFilesCreatedInPast45Minutes(t *testing.T) {
+	serverURL := "http://localhost:8080"
+	client := &http.Client{}
+
+	// Calculate ts for the past 45 minutes
+	endTime := time.Now().Unix()
+	startTime := endTime - 2700
+
+	// Create a GET request with the desired query parameters
+	req, err := http.NewRequest("GET", serverURL+"/alerts?service_id=my_test_service_id&start_ts="+strconv.FormatInt(startTime, 10)+"&end_ts="+strconv.FormatInt(endTime, 10), nil)
+	if err != nil {
+		t.Errorf("Error creating GET request: %v", err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Error sending GET request: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Verify the response status code
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, resp.StatusCode)
+	}
+}
+
+// Error status 400
+func TestGetBadRequest(t *testing.T) {
+	serverURL := "http://localhost:8080"
+	client := &http.Client{}
+
+	// Create a GET request with missing or incorrect query parameters
+	req, err := http.NewRequest("GET", serverURL+"/alerts", nil)
+	if err != nil {
+		t.Errorf("Error creating GET request: %v", err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Error sending GET request: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Verify the response status code
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Expected status code %d, but got %d", http.StatusBadRequest, resp.StatusCode)
+	}
+}
+
+// Error status 404
+func TestGetNotFound(t *testing.T) {
+	serverURL := "http://localhost:8080"
+	client := &http.Client{}
+
+	// Create a GET request for a non-existent resource
+	req, err := http.NewRequest("GET", serverURL+"/nonexistent_resource", nil)
+	if err != nil {
+		t.Errorf("Error creating GET request: %v", err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Error sending GET request: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Verify the response status code
+	if resp.StatusCode != http.StatusNotFound {
+		t.Errorf("Expected status code %d, but got %d", http.StatusNotFound, resp.StatusCode)
+	}
+}
+
+// Error status 500
+func TestGetInternalServerError(t *testing.T) {
+	serverURL := "http://localhost:8080"
+	client := &http.Client{}
+
+	// Define the path to the "alerts" folder
+	alertsFolder := "alerts"
+
+	// Delete the "alerts" folder
+	if err := os.RemoveAll(alertsFolder); err != nil {
+		t.Errorf("Error deleting the 'alerts' folder: %v", err)
+		return
+	}
+
+	// Create a GET request that intentionally triggers an internal server error
+	req, err := http.NewRequest("GET", serverURL+"/alerts?service_id=my_test_service_id&start_ts=0&end_ts=9999999999", nil)
+	if err != nil {
+		t.Errorf("Error creating GET request: %v", err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Error sending GET request: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Verify the response status code
+	if resp.StatusCode != http.StatusInternalServerError {
+		t.Errorf("Expected status code %d, but got %d", http.StatusInternalServerError, resp.StatusCode)
+	}
+
+	// Recreate the "alerts" folder
+	if err := os.MkdirAll(alertsFolder, os.ModePerm); err != nil {
+		t.Errorf("Error recreating the 'alerts' folder: %v", err)
 	}
 }
 
